d26756a5abd14e998fdd0e6ef8672e595f690fbe
Spencer Bloom <spebl@microsoft.com>
2017-11-10 13:19:29 -0800

    add back telemetry with an option to turn it off

---
+++ GoogleTestAdapter/VsPackage.TAfGT/OptionsPages/GeneralOptionsDialogPage.cs
@@ -0,0 +1,21 @@
+﻿// This file has been modified by Microsoft on 11/2017.
+
+using GoogleTestAdapter.Settings;
+
+namespace GoogleTestAdapter.VsPackage.OptionsPages
+{
+
+    public partial class GeneralOptionsDialogPage : NotifyingDialogPage
+    {
+        [LocalizedCategory("CategoryMiscName")]
+        [LocalizedDisplayName("OptionReportNewProjectTelemetry")]
+        [LocalizedDescription("OptionReportNewProjectTelemetryDescription")]
+        public bool ReportNewProjectTelemetry
+        {
+            get { return _reportNewProjectTelemetry; }
+            set { SetAndNotify(ref _reportNewProjectTelemetry, value); }
+        }
+        private bool _reportNewProjectTelemetry = SettingsWrapper.OptionReportNewProjectTelemetryDefaultValue;
+    }
+
+}
