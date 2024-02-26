1a5db3525e194063fe277bf3518ef9fa9392e99e
David Raygoza <davidraygoza@microsoft.com>
2024-02-25 23:46:21 -0800

    Update path in Build.ps1 to put packages in /Packages folder correctly

--- GoogleTestNuGet/Build.ps1
+++ GoogleTestNuGet/Build.ps1
@@ -367,7 +367,7 @@ function Main {
     }
     Invoke-Executable nuget
 
-    $OutputDir = "..\GoogleTestAdapter\Packages"
+    $OutputDir = "TestAdapterForGoogleTest\GoogleTestAdapter\Packages"
 
     Build-BinariesAndNuGet -ToolsetName "v140" -BuildToolset "v141" -DynamicLibraryLinkage $false -DynamicCRTLinkage $true  -OutputDir $OutputDir
     Build-BinariesAndNuGet -ToolsetName "v140" -BuildToolset "v141" -DynamicLibraryLinkage $false -DynamicCRTLinkage $false -OutputDir $OutputDir
