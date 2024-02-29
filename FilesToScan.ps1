519e61a67e5ef00a02042670e5f369b776d3e085
David Raygoza <davidraygoza@microsoft.com>
2024-02-29 14:30:50 -0800

    Create FilesToScanDrop dir if does not exist

--- FilesToScan.ps1
+++ FilesToScan.ps1
@@ -6,11 +6,15 @@ param (
 $filesToScan = @("GoogleTestAdapter.Common.dll", "GoogleTestAdapter.Common.Dynamic.dll", "GoogleTestAdapter.Core.dll", "GoogleTestAdapter.DiaResolver.dll", "GoogleTestAdapter.TestAdapter.dll", "GoogleTestAdapter.VsPackage.TAfGT.dll", "NewProjectWizard.dll", "gtest.dll", "gtestd.dll", "gtest_main.dll", "gtest_maind.dll")
 $FilesToScanDrop = "$buildArtifactStagingDirectory/FilesToScanDrop"
 
+if (!(Test-Path -Path $FilesToScanDrop)) {
+    New-Item -ItemType Directory -Path $FilesToScanDrop | Out-Null
+}
+
 foreach ($file in $filesToScan) {
     $sourcePaths = Get-ChildItem -Path $directoryToSearch -Recurse -Include $file -File
     foreach ($sourcePath in $sourcePaths) {
         $destinationPath = Join-Path $FilesToScanDrop $sourcePath.Name
         Copy-Item $sourcePath.FullName $destinationPath
-        Write-Host "found file to scan: $file"
+        Write-Host "Found File to Scan: $file"
     }
 }
