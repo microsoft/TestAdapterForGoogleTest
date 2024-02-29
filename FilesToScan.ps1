00912236e4cade285b9c0191de9671bb6eb39ac3
David Raygoza <davidraygoza@microsoft.com>
2024-02-29 14:03:43 -0800

    Pass full path of where to search for scannable files

--- FilesToScan.ps1
+++ FilesToScan.ps1
@@ -1,14 +1,16 @@
 param (
-    [string]$buildArtifactStagingDirectory
+    [string]$buildArtifactStagingDirectory,
+    [string]$directoryToSearch
 )
 
 $filesToScan = @("GoogleTestAdapter.Common.dll", "GoogleTestAdapter.Common.Dynamic.dll", "GoogleTestAdapter.Core.dll", "GoogleTestAdapter.DiaResolver.dll", "GoogleTestAdapter.TestAdapter.dll", "GoogleTestAdapter.VsPackage.TAfGT.dll", "NewProjectWizard.dll", "gtest.dll", "gtestd.dll", "gtest_main.dll", "gtest_maind.dll")
 $FilesToScanDrop = "$buildArtifactStagingDirectory/FilesToScanDrop"
 
 foreach ($file in $filesToScan) {
-    $sourcePaths = Get-ChildItem -Path "**/out/binaries/" -Recurse -Include $file -File
+    $sourcePaths = Get-ChildItem -Path $directoryToSearch -Recurse -Include $file -File
     foreach ($sourcePath in $sourcePaths) {
         $destinationPath = Join-Path $FilesToScanDrop $sourcePath.Name
         Copy-Item $sourcePath.FullName $destinationPath
+        Write-Host "found file to scan: $file"
     }
 }
